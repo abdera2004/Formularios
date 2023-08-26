0a94382225c31aa691b232d65f8585be9696f1ee
Ruan <105059155+abdera2004@users.noreply.github.com>
2023-08-26 16:00:21 -0300

    teste

---
+++ exercicio1/script.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $Nome = $_POST['Nome'];
+        $Email = $_POST['Email'];
+        $Idade = $_POST['Idade'];
+        $Nome = $_POST['nome'];
+        $Senha = $_POST['Senha'];
+        $Sexo = $_POST['Sexo'];
+        $Interesses = $_POST['1'];
+        $Interesses = $_POST['2'];
+        $Interesses = $_POST['3'];
+    ?>
+</body>
+</html>
