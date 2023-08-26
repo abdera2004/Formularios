9b9c8b170825fd5adcb8aa83380cdefae5715e37
Ruan <105059155+abdera2004@users.noreply.github.com>
2023-08-26 19:14:50 -0300

    exercicio1 estilizado

--- exercicio1/script.php
+++ exercicio1/script.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
+<div id="principal" class="p-3 position-absolute top-0 start-50 translate-middle-x border border-2 border-black">
     <?php
         $Nome = $_POST['Nome'];
         $Email = $_POST['Email'];
@@ -15,7 +17,7 @@
         $Sexo = $_POST['Sexo'];
         $Mensagem = $_POST['Mensagem'];
         
-        echo "<h2>Informações Digitadas:</h2>";
+        echo "<h2>Informações Digitadas</h2>";
         echo "<p><strong>Nome:</strong> $Nome</p>";
         echo "<p><strong>Email:</strong> $Email</p>";
         echo "<p><strong>Idade:</strong> $Idade</p>";
@@ -25,7 +27,7 @@
         if(isset($_POST["Interesses"]))
             {
                 echo "<strong>Os seus interesses são:</strong><BR>";
-                
+
                 foreach($_POST["Interesses"] as $Interesses)
                 {
                     echo "- " . $Interesses . "<BR>";
@@ -33,9 +35,25 @@
             }
             else
             {
-                echo "<p><strong>Você não escolheu nenhum interesse!</strong><br></p>";
+                echo "<p><strong>Você não escolheu nenhum interesse!</strong></p>";
             }
         echo "<strong>Sua mensagem: </strong>" . $Mensagem;
     ?>
+</div>
+
+<style>
+
+body{
+    background-color: rgb(56, 55, 55);
+}
+#principal{
+        height: 100vh;
+        background-color: rgb(49, 23, 2);
+        color: white;
+
+}
+
+</style>
+
 </body>
 </html>
