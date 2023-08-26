afbbea770bdeee5fcd82dcdb49b6ef67f7dfb9c6
Yaguitoz <daorayago@gmail.com>
2023-08-26 17:10:03 -0300

    dassda

--- exercicio3/script.php
+++ exercicio3/script.php
@@ -7,22 +7,24 @@
 </head>
 <body>
     <?php
-       $imc = $_POST['peso'/('altura'*2)];
+       $peso = $_POST['peso'];
+       $altura = $_POST['altura'];
+       $imc = [$peso/($altura*2)];
 
-       if($imc<16.9){
-        echo 'TU VAI MORRE FI'
+       if($imc<16.9) {
+        echo 'TU VAI MORRE FI';
        } else if($imc >= 17 && <= 18.4){
-        echo 'temq dar uma engordadinha em amigo'
+        echo 'temq dar uma engordadinha em amigo';
        } else if($imc >=18.5 && <=24.9){
-        echo 'ta bem po, nem engorda nem emagrece q ta mec'
+        echo 'ta bem po, nem engorda nem emagrece q ta mec';
        } else if($imc >=25 && <=29.9){
-        echo 'ta meio gordinho em mas ta mec'
+        echo 'ta meio gordinho em mas ta mec';
        } else if($imc >=30 && <=34.9){
-        echo 'ta gordao em vamo querer emagrecer'
+        echo 'ta gordao em vamo querer emagrecer';
        } else if($imc >=35 && <=40){
-        echo 'o amigao tu vai morrer se continuar assim'
+        echo 'o amigao tu vai morrer se continuar assim';
        } else{
-        echo 'AMIGAO AQUI NÃO É LUGAR PRA FAZER COSPLAY DE TAIS CARLA NAO PARA AE'
+        echo 'AMIGAO AQUI NÃO É LUGAR PRA FAZER COSPLAY DE TAIS CARLA NAO PARA AE';
        }
     ?>
 </body>
