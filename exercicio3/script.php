49cdd5f3fc01aa0c7f67e5be3f396f39cbbb3d02
Yaguitoz <daorayago@gmail.com>
2023-08-26 16:47:37 -0300

    sadsa

---
+++ exercicio3/script.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       $imc = $_POST['peso'/('altura'*2)];
+
+       if($imc<16.9){
+        echo 'TU VAI MORRE FI'
+       } else if($imc >= 17 && <= 18.4){
+        echo 'temq dar uma engordadinha em amigo'
+       } else if($imc >=18.5 && <=24.9){
+        echo 'ta bem po, nem engorda nem emagrece q ta mec'
+       } else if($imc >=25 && <=29.9){
+        echo 'ta meio gordinho em mas ta mec'
+       } else if($imc >=30 && <=34.9){
+        echo 'ta gordao em vamo querer emagrecer'
+       } else if($imc >=35 && <=40){
+        echo 'o amigao tu vai morrer se continuar assim'
+       } else{
+        echo 'AMIGAO AQUI NÃO É LUGAR PRA FAZER COSPLAY DE TAIS CARLA NAO PARA AE'
+       }
+    ?>
+</body>
+</html>
