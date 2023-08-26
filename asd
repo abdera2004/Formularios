885ad58c3582677aa9a7a787a47b157b142af5e4
Yaguitoz <daorayago@gmail.com>
2023-08-26 18:12:56 -0300

    dsasa

--- asd
+++ asd
@@ -57,3 +57,26 @@
             }
         }
     ?>
+
+
+function tabuada($tabuada){
+   $cont = 1;
+   $resultado = '';
+
+   while ($cont <= 10){
+        $resultado .= '<tr><td>' . $tabuada . 'x' . $cont . ' = ' . ($tabuada * $cont) . '</td></tr>';
+      $cont++;
+   }
+
+   return $resultado;
+}
+
+$tabela .= '<center><table border="1";>';
+$tabela .= tabuada(10);
+$tabela .= '</table></center>';
+
+echo $tabela;
+
+for( $cont = 1;$cont <= 1; $cont++ ){
+        echo $n*$cont,$n*$cont,$n*$cont,$n*$cont,$n*$cont,$n*$cont,$n*$cont,$n*$cont,$n*$cont,$n*$cont;
+        
--- exercicio4/script.php
+++ exercicio4/script.php
@@ -4,22 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../exercicio3/style.css">
+    <link rel="stylesheet" type="text/css" href="../exercicio4/style.css">
     <title>Document</title>
 </head>
 <body>
 
     <?php
-       $n = $_POST['numero'];
-       
+        $n = $_POST['numero'];
+        $i = 1;
+        while ($i <= 10) {
+            $resultado = $n * $i;
+            echo "<tr>a$resultado a</tr>" ,'<br>';
+            $i++;  
+        } 
 
-       for( $i = 1;$i <= 10; $i++){
-        echo $n*2;
-        
-        
-       }
-
-       
     ?>
+
 </body>
 </html>
