b5e1e6fc8ea08bc07e2d667b92c9e6d55befd85d
abdera2004 <ruanpereira405@gmail.com>
2023-10-21 23:36:24 -0300

    finalizando

--- componentes/rodape.php
+++ componentes/rodape.php
@@ -1,10 +1,7 @@
 <footer>
-    <nav class="navbar" style="background-color: #0095ff;">
+    <nav class="navbar" style="background-color: #000000; color: #ffffff;">
         <div class="container-md d-flex justify-content-evenly">
-            <a class="navbar-brand" href="../exercicio1/index.php">Cadastro</a>
-            <a class="navbar-brand" href="../exercicio2/index.php">Tabuada</a>
-            <a class="navbar-brand" href="../exercicio3/index.php">Triangulo</a>
-            <a class="navbar-brand" href="../exercicio5/index.php">Imc</a>
+            <p>Feito por: Ruan e Yago</p>
         </div>
     </nav>
 </footer>
