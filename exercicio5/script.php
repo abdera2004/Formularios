e7d2e5090d245b4a29460536cafd455a6afcf6a1
abdera2004 <ruanpereira405@gmail.com>
2023-10-21 23:34:52 -0300

    ufa

--- exercicio5/script.php
+++ exercicio5/script.php
@@ -24,48 +24,38 @@ include('./../componentes/cabecalho.php');
 
         if($imc< 16.9){
             echo "<b>TU VAI MORRE LEK MT MAGRO FILHO</b>";
-            exit;
-        }
-        elseif($imc >= 17 && $imc <= 18.4){
+             
+        }elseif($imc >= 17 && $imc <= 18.4){
             echo "<b>temq dar uma engordadinha em amigao</b>";
-            exit;
-        }
-        elseif($imc >= 18.5 && $imc <=24.9){
+        
+        }elseif($imc >= 18.5 && $imc <=24.9){
             echo "<b>ta bem po, nem engorda nem emagrece q ta mec</b>";
-            exit;
-        }
-        elseif($imc >= 25 && $imc <= 29.9){
+            
+        }elseif($imc >= 25 && $imc <= 29.9){
             echo "ta meio gordinho em mas ta mec";
-            exit;
-        }
-        elseif($imc >= 30 && $imc <= 34.9){
+
+        }elseif($imc >= 30 && $imc <= 34.9){
             echo "<b>ta gordao em vamo querer emagrecer</b>";
-            exit;
-        }
-        elseif($imc >= 35 && $imc <= 40){
+
+        }elseif($imc >= 35 && $imc <= 40){
             echo "<b>o amigao tu vai morrer se continuar assim</b>";
-            exit;
-        }
+
+        ?>
+        <?php
+        }else{  
+        ?>
+
+        <?php
+         
+            echo "<B>AMIGAO AQUI NÃO É LUGAR PRA FAZER COSPLAY DE TAIS CARLA NAO PARA AE</B>";    
         
         ?>
+        <img src="<?php echo "../imagens/taiscarla.png" ?>" alt="Minha Imagem" style="height: 150px;" class="position-absolute top-100 start-50 translate-middle-x">
         <?php
-        if($imc< 16.9) {
-            
-                
-        ?>
-
-        <?php
-        }else {
-            echo "<B>AMIGAO AQUI NÃO É LUGAR PRA FAZER COSPLAY DE TAIS CARLA NAO PARA AE</B>";
         }
         ?>
-        <img src="<?php echo "../imagens/taiscarla.png" ?>" alt="Minha Imagem" style="height: 150px;" class="position-absolute top-100 start-50 translate-middle-x">
-
-
     </div> 
     
-
-
 </div>
 
 <?php
